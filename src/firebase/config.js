646bf72716be7c9ccbd353a36a30e77022cf4baf
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-06-24 13:22:04 +0000

    deployement

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxKDlmHxkS1pShAw8ZOUN31f-7LAOqslE",
+  authDomain: "rest-country-api-ffc22.firebaseapp.com",
+  projectId: "rest-country-api-ffc22",
+  storageBucket: "rest-country-api-ffc22.appspot.com",
+  messagingSenderId: "518936709914",
+  appId: "1:518936709914:web:47b97b7e042d68b681baa3"
+};
+
+const app = initializeApp(firebaseConfig);
