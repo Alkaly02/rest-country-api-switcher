81ba2a9ac152e2d65d0c18e29581320f612e0c2b
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-06-25 14:27:33 +0000

    ajout de margin right sur les drapeaux lors de la recherche

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -7,7 +7,10 @@ const AutoComplete = ({countries, searchCountryOnAutoComplete, input}) => {
             countries.filter(country => country.name.common.toUpperCase().startsWith(input.toUpperCase()))
             .map((country, index) => (
                 <div style={{cursor: 'pointer'}} className='my-1' key={`${country.name.common}-${index}`}>
-                    <span onClick={() => searchCountryOnAutoComplete(country.name.common) }> <img style={{width: '20px', fontSize: '0.8rem'}} src={country.flags.png} alt='Crountry flag' /> {country.name.common}</span>
+                    <span onClick={() => searchCountryOnAutoComplete(country.name.common) }>
+                       <img className='me-2' style={{width: '20px', fontSize: '0.8rem'}} src={country.flags.png} alt='Crountry flag' /> 
+                       {country.name.common}
+                    </span>
                 </div> 
             ))
         }
