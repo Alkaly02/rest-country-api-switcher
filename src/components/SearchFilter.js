958c4c1237e37de6605587b07c450bd6b5d9d4b5
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-06-25 09:52:21 +0000

    finission de la mise en place de l'autocompletion

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -6,17 +6,19 @@ const SearchFilter = () => {
     const {countries, setShowCountries} = useCountry()
     const [input, setInput] = useState('');
     const [select, setSelect] = useState('')
-    const [error, setError] = useState('');
     const [searchByRegion, setSearchByRegion] = useState([])
+    // autocompletion
+    const [display, setDisplay] = useState(false);
+    const [search, setSearch] = useState('')
 
     const searchCountry = (e) => {
         e.preventDefault()
+        setDisplay(false)
         const newCountry = searchByRegion.length !== 0 ? 
         searchByRegion.filter(country => country.name.common.toUpperCase() === input.toUpperCase()) :
         countries.filter(country => country.name.common.toUpperCase() === input.toUpperCase());
         
         if(newCountry.length !== 0) {
-            setError('')
             return setShowCountries(newCountry)
         }else{
             return setShowCountries([])
@@ -26,6 +28,12 @@ const SearchFilter = () => {
     const handleSelect = (e) => {
         setSelect(e.target.value)
     }
+
+    const onInputChange = (e) => {
+        setDisplay(true)
+        setInput(e.target.value)
+    }
+    
     useEffect(() => {
         const countryWithSameRegion = countries.filter(country => country.region.toUpperCase() === select.toUpperCase());
         setSearchByRegion(countryWithSameRegion);
@@ -34,22 +42,46 @@ const SearchFilter = () => {
         }
     }, [select, countries, setShowCountries])
 
+    const searchCountryOnAutoComplete = (countryName) => {
+        setInput(countryName);
+        setDisplay(false)
+        const newCountry = searchByRegion.length !== 0 ? 
+        searchByRegion.filter(country => country.name.common.toUpperCase() === countryName.toUpperCase()) :
+        countries.filter(country => country.name.common.toUpperCase() === countryName.toUpperCase());
+        
+        if(newCountry.length !== 0) {
+            return setShowCountries(newCountry)
+        }else{
+            return setShowCountries([])
+        }
+    }
+
   return (
     <div className='d-sm-flex justify-content-between align-items-center position-relative'>
         <div className="form shadow position-relative mb-sm-0 mb-2">
-            <form onSubmit={searchCountry} action="">
+            <form autoComplete='off' onSubmit={searchCountry} action="">
                 <FaSearchLocation className='position-absolute search' />
                 <input
                 value={input}
-                onChange={(e) => setInput(e.target.value)}
+                onChange={onInputChange}
                 placeholder='Search for a country'
                 type="search" 
                 id="search" 
                 className="form-control px-5" 
                 aria-describedby="searchHelp" />
             </form>
+            {/* autoComplete */}
             {
-                error && <div style={{zIndex: '999', left: '45%'}} className='bg-danger p-2 position-absolute text-light'>{error}</div>
+                (display && input !== '' ) && <div style={{zIndex: '999', left: '45%'}} className='bg-light p-2 position-absolute text-dark'>
+                        {
+                            countries.filter(country => country.name.common.toUpperCase().startsWith(input.toUpperCase()))
+                            .map((country, index) => (
+                                <div key={`${country.name.common}-${index}`}>
+                                    <span onClick={() => searchCountryOnAutoComplete(country.name.common) }>{country.name.common}</span>
+                                </div> 
+                            ))
+                        }
+                    </div>
             }
         </div>
         <div className="select shadow">
