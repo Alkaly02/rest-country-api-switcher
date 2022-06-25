2a1d98f7822ffaa4f9f27e454d9bf208c02a9b08
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-06-25 14:41:21 +0000

    remplacer le texte de EmptyList de fr en en

--- src/components/EmtyList.js
+++ src/components/EmtyList.js
@@ -5,7 +5,7 @@ const EmtyList = () => {
   return (
     <div className='text-center mt-5'>
         <img src={EmptyList} alt="" />
-        <p>Ce pays ne fait pas partie de cette region</p>
+        <p>This country is not part of this region</p>
     </div>
   )
 }
