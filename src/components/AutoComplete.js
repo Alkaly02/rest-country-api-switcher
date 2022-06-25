a342a0f5e8e81d2b3aeb03b8ba047ff26e822e3c
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-06-25 14:30:47 +0000

    ajout de margin bottom and tom sur le div

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -6,7 +6,7 @@ const AutoComplete = ({countries, searchCountryOnAutoComplete, input}) => {
         {
             countries.filter(country => country.name.common.toUpperCase().startsWith(input.toUpperCase()))
             .map((country, index) => (
-                <div style={{cursor: 'pointer'}} className='my-1' key={`${country.name.common}-${index}`}>
+                <div style={{cursor: 'pointer'}} className='my-2' key={`${country.name.common}-${index}`}>
                     <span onClick={() => searchCountryOnAutoComplete(country.name.common) }>
                        <img className='me-2' style={{width: '20px', fontSize: '0.8rem'}} src={country.flags.png} alt='Crountry flag' /> 
                        {country.name.common}
